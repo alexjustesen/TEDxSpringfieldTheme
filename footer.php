c1e86f306ad014111f541291e7af680679bd01cf
Alex Justesen <alex@alexjustesen.com>
2016-09-26 18:08:39 -0400

    updated formatting

--- footer.php
+++ footer.php
@@ -1,25 +1,25 @@
 <?php $template_url = get_template_directory_uri(); ?>
 
 <div class="black-bg spacing-top">
-  <div class="container">
-    <footer>
-      <div class="row">
-        <div class="col-md-4 col-sm-12 legal">
-          This TEDx event is independently organized.<br>
-          Copyright © <?= get_theme_mod('event_name', 'TEDx') ?> <?= date("Y"); ?>. All Rights Reserved. <a
-            href="/legal-and-privacy/">Legal &amp; Privacy</a>
-        </div>
-        <div class="col-md-3 col-sm-12">
-          <?= get_theme_mod('twitter_follow_button'); ?>
-        </div>
-        <div class="col-md-5 col-sm-12 web-partners text-right">
-          <div class="built-by gutter-right gutter-bottom">Built by <a href="//alexjustesen.com">Alex Justesen</a></div>
-        </div>
-      </div>
+    <div class="container">
+        <footer>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 legal">
+                    This TEDx event is independently organized.<br>
+                    Copyright © <?= get_theme_mod('event_name', 'TEDx') ?> <?= date("Y"); ?>. All Rights Reserved. <a href="/legal-and-privacy/">Legal &amp; Privacy</a>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <?= get_theme_mod('twitter_follow_button'); ?>
+                </div>
+                <div class="col-md-5 col-sm-12 web-partners text-right">
+                    <div class="built-by gutter-right gutter-bottom">Built by <a href="//alexjustesen.com">Alex Justesen</a></div>
+                </div>
+            </div>
 
-    </footer>
-  </div>
+        </footer>
+    </div>
 </div>
+
 <?php wp_footer(); ?>
 
 <!-- Compiled JS -->
