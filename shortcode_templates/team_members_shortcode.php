fda54d35886cf5ed6483e2b32945c3dc5a9e5da3
Alex Justesen <alex@alexjustesen.com>
2017-09-27 20:44:28 -0400

    updated team member shortcode ui

--- shortcode_templates/team_members_shortcode.php
+++ shortcode_templates/team_members_shortcode.php
@@ -1,70 +1,64 @@
 <section>
     
-    <div class="row animated-tiles">
+    <div class="row">
         
-        <?php if ($team_members->have_posts()): while ($team_members->have_posts()) : $team_members->the_post(); ?>
-        <?php $post = get_post(); ?>
+        <?php if ( $team_members->have_posts() ) : while ( $team_members->have_posts() ) : $team_members->the_post(); ?>
 
-        <?php
-            $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'team');
-            if (is_array($thumb) && !empty($thumb[0])) {
-                $thumbnail_src = $thumb[0];
-            } else {
-                $thumbnail_src = get_bloginfo('template_url') . "/dist/img/defaults/team-placeholder.jpg";
-            }
-        ?>
+            <?php $post = get_post(); ?>
+
+            <?php
+                // Set team member thumbnail
+                $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'team');
         
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 team-tile" data-remote="true" data-href="<?php the_permalink(); ?>">
+                if (is_array($thumb) && !empty($thumb[0])) {
+                    $thumbnail_src = $thumb[0];
+                } else {
+                    $thumbnail_src = get_bloginfo('template_url') . "/dist/img/defaults/team-placeholder.jpg";
+                }
+        
+                // Set team member twitter
+                $team_links = array();
+        
+                if ( get_post_meta( $post->ID, '_team_twitter_link', true ) ) {
+                    $team_links['twitter'] = get_post_meta($post->ID, '_team_twitter_link', true);
+                }
+            ?>
 
-            <div class="team-photo-wrapper">
-                
-                <img alt='<?php the_title(); ?>' src='<?=  $thumbnail_src; ?>' class="team-photo img-responsive">
-                                   
-                <?php
-                    $team_links = array();
-                
-                    if ( get_post_meta($post->ID, '_team_twitter_link', true) ) {
-                        $team_links['twitter'] = get_post_meta($post->ID, '_team_twitter_link', true); }
-                ?>
-                    
-                <?php if ( !empty( $team_links ) ): ?>
-
-                <div class="team-links">
-                    
-                    <?php if (!empty($team_links['twitter'])): ?>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 team-tile" data-remote="true" data-href="<?php the_permalink(); ?>">
+            
+                <div class="row">
+                   
+                    <div class="col-xs-12">
+                       
+                        <img src="<?= $thumbnail_src; ?>" alt="<?php the_title(); ?>" class="team-photo img-responsive img-circle">
                         
-                        <a href="<?=$team_links['twitter']?>" target="_blank" title="<?=$post->post_title?>'s Twitter"><i class="fa fa-twitter fa-fw"></i></a>
-                        
-                    <?php endif; ?>
-
-                </div>
-                
-                <?php endif; ?>
-                <!-- .team-links -->
-                
-            </div>
-            <!-- .team-photo-wrapper -->
-
-            <div class="team-info">
-
-                <div class="team-title">
+                    </div>
                     
-                    <h2><?=$post->post_title?></h2>
-
-                    <div class="team-role" alt="<?=  get_post_meta($post->ID, '_team_job_description', true) ?>">
-                        <?=  get_post_meta($post->ID, '_team_job_description', true) ?>
+                    <div class="col-xs-12">
+                       
+                        <h4 class="text-center" title="<?= $post->post_title; ?>">
+                            <?= $post->post_title; ?><br/>
+                            <small><?= get_post_meta($post->ID, '_team_job_description', true); ?></small>
+                        </h4>
+                        
+                        <?php if ( !empty( $team_links ) ) : ?>
+                        <p class="text-center">
+                            <?php if ( !empty( $team_links['twitter'] ) ) : ?>
+                                <a href="<?= $team_links['twitter']; ?>" title="<?= $post->post_title; ?>'s Twitter" target="_blank">
+                                    <i class="fa fa-twitter fa-fw"></i>
+                                </a>
+                            <?php endif; ?>
+                        </p>
+                        <?php endif; ?>
+                        
                     </div>
                     
                 </div>
-                <!-- .team-title -->
 
             </div>
-            <!-- .team-info -->
 
-        </div>
-        
         <?php endwhile; endif; ?>
-        
+
     </div>
-    
+
 </section>
