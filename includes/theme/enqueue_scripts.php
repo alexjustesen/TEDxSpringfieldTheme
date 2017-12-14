90931ce1cb2673dda9282b2a082eaa3cee9f3d52
Alex Justesen <alex@alexjustesen.com>
2017-12-13 21:22:40 -0500

    updated scripts to v1.3.0-beta2

--- includes/theme/enqueue_scripts.php
+++ includes/theme/enqueue_scripts.php
@@ -7,12 +7,12 @@ if (!function_exists('TEDx_scripts')) :
         wp_deregister_script( 'jquery' );
 
         // enqueue javascript files    
-        wp_enqueue_script( 'vendor', get_template_directory_uri() . '/dist/js/vendor.min.js', array(), '1.3.0-beta', true );            
-        wp_enqueue_script( 'application', get_template_directory_uri() . '/dist/js/application.min.js', array(), '1.3.0-beta', true );
+        wp_enqueue_script( 'vendor', get_template_directory_uri() . '/dist/js/vendor.min.js', array(), '1.3.0-beta2', true );            
+        wp_enqueue_script( 'application', get_template_directory_uri() . '/dist/js/application.min.js', array(), '1.3.0-beta2', true );
 
         //enqueue css files
-        wp_enqueue_style( 'vendor', get_template_directory_uri() . '/dist/css/vendor.min.css', array(), '1.3.0-beta', 'all' );
-        wp_enqueue_style( 'application', get_template_directory_uri() . '/dist/css/application.min.css', array(), '1.3.0-beta', 'all' );
+        wp_enqueue_style( 'vendor', get_template_directory_uri() . '/dist/css/vendor.min.css', array(), '1.3.0-beta2', 'all' );
+        wp_enqueue_style( 'application', get_template_directory_uri() . '/dist/css/application.min.css', array(), '1.3.0-beta2', 'all' );
     }
 
     add_action( 'wp_enqueue_scripts', 'TEDx_scripts' );
