da1a6e0478500ba5440bc059d9fd068da215eae5
Alexander Justesen <alex@alexjustesen.com>
2018-09-05 17:14:25 -0400

    added column ids and cleaned up template file

--- home.php
+++ home.php
@@ -1,67 +1,49 @@
-<?php
-get_header();
-$query = new TEDxQuery();
-$sticky_posts = $query->sticky_posts(5);
-$unsticky_posts = $query->unsticky_posts();
+<?php // home.php
+    get_header();
+
+    $query = new TEDxQuery();
+    $unsticky_posts = $query->unsticky_posts();
 ?>
 
-<?php if (count($sticky_posts) > 0 && $query->current_page() === 1): ?>
-  <div class="sticky-posts">
+<div class="page-contents">
+    
     <div class="container">
-      <div class="row">
-        <?php
-        // If there is only one featured (sticky) post
-        if (count($sticky_posts) === 1) {
-          require('partials/blog/_single_sticky.php');
-        }
-        // If there are more than one featured (sticky) post
-        if (count($sticky_posts) > 1) {
-          require('partials/blog/_grouped_stickies.php');
-        }
-        ?>
+        
+        <div class="row">
+            
+            <div id="main" class="col-md-9">
+                
+                <div class="page-header">
+                    <h3>Recent Posts</h3>
+                </div>
+                
+                <?php
+                    if ( count( $unsticky_posts ) > 0) {
+                        global $post;
+                        
+                        foreach ( $unsticky_posts as $index => $post ) {
+                            setup_postdata($post);
+                            WP_Render::partial('partials/blog/_post_excerpt.php');
+                        }
+                    }
+                    else {
+                        WP_Render::partial('partials/_not_found.php', ['message' => 'There does not appear to be any posts here...']);
+                    }
+                ?>
 
-      </div>
-      <!-- .row -->
-    </div>
-    <!-- .container -->
-  </div><!-- .sticky-posts -->
-<?php endif; ?>
-
-
-  <div class="container spacing-top">
-    <div class="row">
-
-      <div class="col-md-9">
-
-        <div class="page-header">
-          <h4>Recent</h4>
+                <?php WP_Render::partial('partials/_pagination.php', ['offset' => count( $unsticky_posts ) ]); ?>
+                
+            </div>
+            <div id="sidebar" class="col-md-3">
+                
+                <?php get_sidebar(); ?>
+                
+            </div>
+            
         </div>
-        <!-- .page-header -->
-
-        <?php
-        if (count($unsticky_posts) > 0):
-          global $post;
-          foreach ($unsticky_posts as $index => $post):
-            setup_postdata($post);
-            WP_Render::partial('partials/blog/_post_excerpt.php');
-          endforeach;
-        else:
-          WP_Render::partial('partials/_not_found.php', ['message' => 'There does not appear to be any posts here...']);
-        endif;
-        ?>
-
-        <?php WP_Render::partial('partials/_pagination.php', ['offset' => count($sticky_posts)]); ?>
-
-      </div>
-      <!-- .col-md-8 -->
-
-      <div class="col-md-3">
-        <?php get_sidebar(); ?>
-      </div>
-      <!-- .col-md-4 -->
-
+        
     </div>
-    <!-- .row -->
-  </div><!-- .container -->
+    
+</div>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,14 +1,26 @@
-<?php
-/*
-Template Name: One column (Default)
-*/
-get_header();
+<?php // Template Name: One column (Default)
+    get_header();
 ?>
-  <div class="container contents spacing-top">
-    <section>
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php the_content(); ?>
-      <?php endwhile; endif; ?>
-    </section>
-  </div><!-- .container contents -->
+<div class="page-contents">
+   
+    <div class="container">
+    
+        <div class="row">
+            
+            <div id="main" class="col-md-12">
+                <section>
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        
+                        <?php the_content(); ?>
+                        
+                    <?php endwhile; endif; ?>
+                </section>
+            </div>
+            
+        </div>
+        
+    </div>
+    
+</div>
+
 <?php get_footer(); ?>
