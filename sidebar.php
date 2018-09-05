10fafeb3e1a43a8ed50813c26701aa44f98c838e
Alexander Justesen <alex@alexjustesen.com>
2018-09-05 15:51:05 -0400

    renamed sidebars

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,15 @@
 <div id="sidebar">
-  <ul>
-    <?php
-    if (is_page_template('page_templates/template_home.php')) {
-      dynamic_sidebar('home-sidebar');
-    } elseif (is_page()) {
-      dynamic_sidebar('page-sidebar');
-    } else {
-      dynamic_sidebar('blog-sidebar');
-    }
-    ?>
-  </ul>
+    <ul>
+        <?php
+            if ( is_page_template( 'page_templates/template_home.php' ) ) {
+              dynamic_sidebar('sidebar-home');
+            }
+            elseif ( is_page() ) {
+              dynamic_sidebar('sidebar-page');
+            }
+            else {
+              dynamic_sidebar('sidebar-blog');
+            }
+        ?>
+    </ul>
 </div>
